d3c10f3bb0a623b2f1aea1af0a12d491e5cdebaa
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-05-06 12:09:03 -0300

    variaveis e tipos de dados

---
+++ variaveis.py
@@ -0,0 +1,24 @@
+nome_user = 'Lucas'
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Lucas', 30
+estado = True
+
+# funcao type()
+print(type(media))
+print(type(nome))
+print(type(n1))
+print(type(estado))
+print(type(1+2j))
+
+# funcao isinstance()
+a = 10
+b = 'Sol'
+print(isinstance(a, int))
+print(isinstance(a, (int, float)))
+print(isinstance(b, float))
+
+a = 50
+c = 3
+r = a * c
+print(r)
